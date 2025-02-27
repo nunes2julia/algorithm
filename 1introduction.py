77d36fb48403e9c9f8254f2af77431833887f2da
Julia Nunes <141152715+nunes2julia@users.noreply.github.com>
2025-02-27 16:39:44 -0300

    intro

---
+++ 1introduction.py
@@ -0,0 +1,16 @@
+
+ 
+""" 
+def count_even(numbers):
+    result = 0
+    for x in numbers:
+        if x % 2 == 0:
+            result += 1
+    return result
+
+   
+    print(count_even([1, 2, 3])) # 1
+    print(count_even([2, 2, 2, 2, 2])) # 5
+    print(count_even([5, 4, 1, 7, 9, 6])) # 2
+""""
+
