2a3e12f79ce8a5fa5e7ed91a696ac92e10c9225e
Julia Nunes <141152715+nunes2julia@users.noreply.github.com>
2025-02-27 17:02:33 -0300

    intro 1.2

--- 1introduction.py
+++ 1introduction.py
@@ -31,6 +31,25 @@ def max_diff(numbers):
     return max(numbers) - min(numbers)
 
 # Medicao de eficiencia
+# programa que testa a eficiencia da max_diff funcao :
 
+import random
+import time
+
+def max_diff(numbers):
+
+    n = 1000
+    print("n:", n)
+    random.seed(1337)
+    numbers = [random.randint(1, 10**6) for _ in range(n)]
+
+    start_time = time.time()
+    result = max_diff(numbers)
+    end_time = time.time()
+
+    print("result:", result)
+    print("time:", round(end_time - start_time, 2), "s")
+
+    
 
 
