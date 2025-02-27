4ead0fd481e70ddd1b03e104f812bcfaf8276c4e
Julia Nunes <141152715+nunes2julia@users.noreply.github.com>
2025-02-27 16:58:32 -0300

    intro 1.1

--- 1introduction.py
+++ 1introduction.py
@@ -1,4 +1,4 @@
-
+#algoritmo?
 
 def count_even(numbers):
     return sum (x % 2 == 0 for x in numbers)
@@ -7,5 +7,30 @@ def count_even(numbers):
     print(count_even([1, 2, 3])) # 1
     print(count_even([2, 2, 2, 2, 2])) # 5
     print(count_even([5, 4, 1, 7, 9, 6])) # 2
+
+    #Eficiência dos algoritmos
+
+# Algoritimo 1
+
+def max_diff(numbers):
+    result = 0
+    for x in numbers:
+        for y in numbers:
+            result = max(result, abs(x - y))
+    return result
+
+# Algoritimo 2
+
+def max_diff(numbers):
+    numbers = sorted(numbers)
+    return numbers[-1] - numbers[0]
+
+# Algoritmo 3
+
+def max_diff(numbers):
+    return max(numbers) - min(numbers)
+
+# Medicao de eficiencia
+
 
 
