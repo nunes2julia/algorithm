dcfcb8b96ae35e1169897616b1975be277b65f27
Julia Nunes <141152715+nunes2julia@users.noreply.github.com>
2025-04-14 16:57:55 -0300

    4.1

--- 4. Hashing.py
+++ 4. Hashing.py
@@ -48,3 +48,125 @@ numbers.remove(2)
 print(nummbers) # {1, 3}
 
 # List vs set
+
+#    Efficiency                       List                  Set
+# Operation Adding ( append /add )    O(1)                  O(1)
+# Finding ( in )                      O(n)	                O(1)
+# Removing ( remove )                 O(n)                  O(1)
+
+# Indexing
+# in a list, elements can be accessed using index.
+
+numbers = [1. 2. 3]
+print(numbers[1]) #2
+
+# A Set dos not support indexing:
+
+numbers = set([1, 2, 3])
+print(numbers[1]) # TypeError: 'set' object is not subscriptable
+
+# Repeated elements
+#In a list, an element can occur multiple times:
+
+numbers = []
+number.append(5)
+number.append(5)
+number.append(5)
+
+print(numbers) # {5}
+
+# Task
+
+#You are given a list of numbers. How many distinct numbers does it contain?
+#For example, when the list is [3,1,2,1,5,2,2,3], the desired answer is 
+# 4 because the distinct numbers are 1, 2, 3 and 5
+
+# slow solution (list)
+def count_distinct(numbers):
+    seen = []
+    for x in numbers:
+        if x not in seen:
+            seen.append(x)
+    return len(seen)
+
+# Efficient solution (set)
+
+def count_distinct(numbers):
+    seen = set()
+    for x in numbers:
+        seen.add(x)
+    return len(seen)
+
+# shorten code
+
+def count_distinct(numbers):
+    return len(set(numbers))
+
+
+# Dictionary
+
+# Example (dictionary weights)
+
+weights = {}
+
+weights["apina"] = 100
+weights["banaani"] = 1
+weights["cembalo"] = 500
+
+weights = {"apina": 100, "banaani": 1, "cembalo": 500}
+
+# elements of a list:
+
+print(weights["apina"]) # 100
+weights["apina"] = 150
+print(weights["apina"]) # 150
+
+# operator 'in' checks if a given key is in the dictionary:
+
+print("apina" in weights) # True
+print("ananas" in weights) # False
+
+# The comand 'del' removes a key
+
+print(weights) # {'apina': 100, 'banaani': 1, 'cembalo': 500}
+del weights["banaani"]
+print(weights) # {'apina': 100, 'cembalo': 500}
+
+# Using a  dictionary
+# Has an element occured
+# keep track of elements that have been seen
+
+seen = {}
+for x in items:
+    seen[x] = True
+
+# same functionality
+
+seen = set()
+for x in items:
+    seen.add(x)
+
+# Counting occurrences
+
+count = {}
+for x in items:
+    if x not in count:
+        count[x] = 0 
+    count[x] += 1
+
+# Position of occurrence
+
+pos = {}
+for i, x in enumerate(items):
+    pos[x] = i
+
+# TASK 2
+
+You are given a list of numbers, and your task is to compute the mode, which is the most 
+frequent number on the list. If the mode is not unique, you can choose any of the possible 
+choices for the most frequent number.
+For example, when the list is [1,2,3,2,2,3,2,2], the desired answer is 2.
+
+def find_mode(numbers):
+
+    
